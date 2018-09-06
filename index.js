9857335ee2061ae14af3a36ee88d68143c6a1b31
Johnathon Wood <jaywood128@gmail.com>
2018-09-06 17:04:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var kittens = ["Milo" , "Otis" , "Garfield"]//
- function destructivelyAppendKitten(name) { 
-     kittens.push(name); 
+ function destructivelyAppendKitten(name) {
+     kittens.push(name);
 }
   function destructivelyPrependKitten(name) {
-    kittens.unshift(name); 
+    kittens.unshift(name);
   }
-
-
